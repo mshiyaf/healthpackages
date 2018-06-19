b689a6e489d58aa0fab51df35f2e4a7ba5b3689e
mariapauly <mariapauly40@gmail.com>
2018-06-19 12:44:55 +0530

    s

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ Route::post('select-ajax', 'PackagesController@selectAjax');
 
 Route::post('/packages','PackagesController@store');
 
-Route::resource('edit','PackagesController');
+Route::get('/edit/{id}','PackagesController@edit');
 
-// Route::get('/delete/{id}','PackagesController@delete');
+Route::post('/update','PackagesController@update');
+
+Route::get('/delete/{id}','PackagesController@delete');
